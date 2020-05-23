953b230901e61023f7bedefe834656a30e20341a
VegimShala <vegim.shala@studentet.uni-pr.edu>
2020-05-23 21:25:45 +0200

    no message

--- simora/src/main/java/unipr/fshmn/simora/controllers/RestController.java
+++ simora/src/main/java/unipr/fshmn/simora/controllers/RestController.java
@@ -1,9 +1,18 @@
 package unipr.fshmn.simora.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    @GetMapping("/addUser")
+    public ModelAndView addUser()
+    {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("addUser");
+        return mv;
+    }
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
