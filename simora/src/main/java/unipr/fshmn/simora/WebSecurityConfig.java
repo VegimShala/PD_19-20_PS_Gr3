cd8bdfdcc52ca23e2310e634de97e2854ca127e4
Aurora Bytyqi <37541986+aurorabytyqi@users.noreply.github.com>
2020-05-25 17:45:09 +0200

    logout

--- simora/src/main/java/unipr/fshmn/simora/WebSecurityConfig.java
+++ simora/src/main/java/unipr/fshmn/simora/WebSecurityConfig.java
@@ -42,6 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
+                        .logoutSuccessUrl("/login")
                 .permitAll();
     }
 
